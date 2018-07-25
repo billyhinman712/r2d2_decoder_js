3f27dbf5f1b4983e2c75a13eb344800344d36016
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-07-25 09:48:56 -0700

    nice and easy

---
+++ solutions.js
@@ -0,0 +1,49 @@
+var beeps = 2;
+var boops = 6;
+var total = beeps + boops;
+console.log(total); //prints 8 H
+
+var beeps = 0;
+var boops = 5;
+var total = beeps + boops;
+console.log(total); //prints 5 E
+
+var beeps = 9;
+var boops = 3;
+var total = beeps + boops;
+console.log(total); // prints 12 L
+
+var beeps = 4;
+var boops = 8;
+var total = beeps + boops;
+console.log(total); //prints 12 L
+
+var beeps = 10;
+var boops = 5;
+var total = beeps + boops;
+console.log(total); //prints 15 O
+
+var beeps = 11;
+var boops = 12;
+var total = beeps + boops;
+console.log(total); //prints 23 W
+
+var beeps = 5;
+var boops = 5;
+var total = beeps + boops;
+console.log(total); //prints 10 J
+
+var beeps = 1;
+var boops = 17;
+var total = beeps + boops;
+console.log(total); //prints 18 R
+
+var beeps = 5;
+var boops = 7;
+var total = beeps + boops;
+console.log(total); //prints 12 L
+
+var beeps = 4;
+var boops = 0;
+var total = beeps + boops;
+console.log(total); //prints 4 D
